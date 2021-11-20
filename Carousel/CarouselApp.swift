3fbf6da4f833f3f1ee424613de4afcbd0638b9d0
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-11-20 17:21:52 +0300

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Kazakova Nataliya on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
